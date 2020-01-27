ebec808c0a8d1d0ad812d23df5dedb0eff5b82b3
Tyler <tylersayvetz@gmail.com>
2020-01-27 13:35:00 -0800

    finish schema

--- schema.sql
+++ schema.sql
@@ -1,24 +1,27 @@
 DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS libraries;
+DROP TABLE IF EXISTS books_in_libraries;
 
-CREATE TABLE IF NOT EXISTS books (
-    bookid SERIAL PRIMARY KEY,
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
     author VARCHAR(255),
     timeline VARCHAR(255),
     descript TEXT,
     title VARCHAR (255)
 );  
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     usernames VARCHAR(255),
-    libraryid INT not null
+    library_id INT not null
 );
 
-CREATE TABLE IF NOT EXISTS libraries (
-    libraryid PRIMARY KEY,
-    bookid INT not null,
-    bookmark INT not null
+CREATE TABLE books_in_libraries (
+    library_id SERIAL PRIMARY KEY,
+    book_id INT not null,
+    bookmark INT not null,
+    user INT not null
 );  
+
+
     
