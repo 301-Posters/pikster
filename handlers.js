231b0e4d48bbccccc7f282eb754c6fc4d9829d3e
Allyson Reyes <areyes986@gmail.com>
2020-01-27 20:25:50 -0800

    added routes and a hander.js to server

---
+++ handlers.js
@@ -0,0 +1,43 @@
+////////////////////////MODULES////////////////////////
+
+
+
+const generateMovie = (request, response) => {
+  console.log('some stuff');
+
+}
+
+const createAcc = (request, response) => {
+  console.log('some stuff');
+}
+
+const generateLibrary = (request, response) => {
+  console.log('some stuff');
+
+}
+
+const secureLogin = (request, response) => {
+  console.log('some stuff');
+
+}
+
+const deleteMovie = (request, response) => {
+  console.log('some stuff');
+
+}
+
+const updateLibrary = (request, response) => {
+  console.log('some stuff');
+
+}
+
+
+
+module.exports = {
+  generateMovie: generateMovie,
+  createAcc: createAcc,
+  generateLibrary: generateLibrary,
+  secureLogin: secureLogin,
+  deleteMovie: deleteMovie,
+  updateLibrary: updateLibrary
+};
